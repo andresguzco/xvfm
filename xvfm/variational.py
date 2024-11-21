d2a521fd8dcb1629d6a55af15e86a3b6cb057237
Andres <andresguzco@gmail.com>
2024-11-21 10:27:25 -0500

    EOD commit

--- xvfm/variational.py
+++ xvfm/variational.py
@@ -22,6 +22,7 @@ class GaussianVariationalDist(VariationalDist):
         identity = torch.eye(mu.size(1)).to(mu.device).unsqueeze(0).expand(mu.size(0), -1, -1)
         t = t.unsqueeze(1).expand(-1, mu.size(1), mu.size(1))
         t = torch.clamp(t, 0, 1)
-        # covariance = (1 - (1 - 0.01) * t) * identity
-        covariance = ((1 - t)**2) * identity
+        covariance = ((1 - (1 - 0.01) * t)**2) * identity
+        # covariance = (1/2) * identity
+        # covariance = identity
         return torch.distributions.MultivariateNormal(mu, covariance)
